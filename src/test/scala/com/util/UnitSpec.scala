bbf792c7cfe1f62a34f8b4cb6f55bba06afa8163
Prasad Sriramula <prasad.sriramula@digital.hmrc.gov.uk>
2016-08-19 07:35:38 +0100

    Adding test resouces

---
+++ src/test/scala/com/util/UnitSpec.scala
@@ -0,0 +1,8 @@
+package com.util
+
+/**
+ * Created by prasadsriramula on 19/08/2016.
+ */
+import org.scalatest._
+abstract class UnitSpec extends FlatSpec with Matchers with
+OptionValues with Inside with Inspectors
