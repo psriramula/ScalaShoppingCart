a4b9e9a21a48bb2625af5a1a84f1aacfb0170d47
Prasad Sriramula <prasad.sriramula@digital.hmrc.gov.uk>
2016-08-19 07:43:55 +0100

    Adding Initial Model

---
+++ src/main/scala/com/shoppingcart/model/Catalogue.scala
@@ -0,0 +1,17 @@
+package com.shoppingcart.model
+
+/**
+ * Created by prasadsriramula on 19/08/2016.
+ */
+sealed trait CatalogueItem {
+  def price: Double
+}
+
+object Apple extends CatalogueItem {
+  override def price: Double = 0.60
+}
+
+object Orange extends CatalogueItem {
+  override def price: Double = 0.25
+}
+
